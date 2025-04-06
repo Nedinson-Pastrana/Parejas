3b54260d9d11d3bbb01eaa364d57fe08ce5aa6e6
Nedinson Pastrana <harryjakoban@gmail.com>
2025-04-06 12:11:42 -0500

    parejas
    parejas

---
+++ parejas/script.js
@@ -0,0 +1,19 @@
+
+// permite que el modal funcione
+const modal = document.getElementById("modal");
+const openModalBtn = document.getElementById("openModalBtn");
+const closeModalBtn = document.getElementById("closeModalBtn");
+
+openModalBtn.onclick = function () {
+  modal.style.display = "block";
+};
+
+closeModalBtn.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
